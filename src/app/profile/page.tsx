9a1a30bb68c6665bb2132864556378e5784268a4
Kartik Makhloga <90948334+KartikMakhloga@users.noreply.github.com>
2023-09-06 13:08:54 +0530

    added middleware and me route

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,30 +3,57 @@ import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { toast } from "react-hot-toast";
+import React, { useEffect, useState } from "react";
 
 export default function ProfilePage() {
-  const router = useRouter()
+  const router = useRouter();
+  const [data, setData] = useState("nothing");
   const onlogout = async () => {
     try {
       await axios.get("/api/users/logout");
-      toast.success('Logout successful');
-      router.push('/login')
+      toast.success("Logout successful");
+      router.push("/login");
     } catch (error: any) {
       console.log(error.message);
       toast.error(error.message);
     }
   };
 
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
+
+  //getting data with useEffect
+
+  // useEffect(()=>{
+  //   getUserDetails();
+  // },[data])
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
       <hr />
       <p>Profile page</p>
+      <h2 className="padding rounded bg-green-500">
+        {data === "nothing" ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
       <button
         onClick={onlogout}
         className="bg-blue-500 mt-4 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded"
       >
         Logout
+      </button>
+      <button
+        onClick={getUserDetails}
+        className="bg-purple-500 mt-4 hover:bg-purple-800 text-white font-bold py-2 px-4 rounded"
+      >
+        getUserDetails
       </button>
     </div>
   );
